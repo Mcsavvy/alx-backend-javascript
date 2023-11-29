a9ab03abd0bdc682082a181accb8fa446b300fff
mcsavvy <davemcsavvii@gmail.com>
2023-11-29 15:53:24 +0100

    fixes(0x00): Fixed lint errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let i = 0;
+  const newArray = [];
   for (const value of array) {
-    array[i] = appendString + value;
-    i++;
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
